3dd5a55076fb54a79819d481560d83bb49bea3f5
mmamuka <mmamuka020@gmail.com>
2022-08-12 16:52:21 +0400

    first commit

---
+++ mailinput.js
@@ -0,0 +1,23 @@
+
+// $(document).ready(function(){
+    
+//     });
+    
+// email {
+// const email = document.querySelector('.emailInput');
+// const submit = document.querySelector('button');
+// const error = document.querySelector('#error');
+
+// const showError = () => {
+//     error.style.display = 'block';
+// };
+
+// submit.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if (!email.value.match(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/)) {
+//         return showError();
+//     }
+
+//     console.log('success');
+// });
+// }
